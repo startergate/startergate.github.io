6cbc91694e70c1054ab82b76ce4ffd32ab1bb3a2
startergate <startergate36@gmail.com>
2020-11-12 21:14:58 +0900

    refac: improve layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -33,7 +33,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
+        <main className='main'>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
