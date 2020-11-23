19122440b630f25160e59782aa7aa1fab8d87987
startergate <startergate36@gmail.com>
2020-11-23 18:05:09 +0900

    refac: 프로젝트 언어 뱃지 분리

---
+++ src/components/projects/projectLanguage.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+import LanguageColor from '../../enums/langs';
+
+import './projectLanguage.css';
+
+const LanguageBadge = ({ tag }) => {
+  return (
+    <span className="project-language">
+      <span
+        className="project-language-badge"
+        style={{
+          backgroundColor: LanguageColor[tag] || '#ccc',
+        }}
+      />
+      {tag}
+    </span>
+  );
+};
+
+export default LanguageBadge;
--- src/components/projects/simple.tsx
+++ src/components/projects/simple.tsx
@@ -3,25 +3,29 @@ import * as React from 'react';
 import './simple.css';
 import LanguageColor from '../../enums/langs';
 import Badge from '../images/badge';
+import ProjectDetail from './detail';
+import * as ReactDOM from 'react-dom';
+import LanguageBadge from './projectLanguage';
+
+const overlayDispatcher = (event: React.MouseEvent) => {
+  const overlayHandler = document.querySelector('.overlay');
+  overlayHandler.innerHTML = '';
+  ReactDOM.render(
+    <ProjectDetail id={event.currentTarget.id} />,
+    overlayHandler
+  );
+};
 
 const ProjectCard = ({ data }) => {
   return (
-    <div className="project-card" id={data.id}>
+    <div className="project-card" id={data.id} onClick={overlayDispatcher}>
       <div className="project-card-name">
         <Badge className="project-card-badge" src={data.imgSrc} />
         {data.name}
       </div>
       <div className="project-card-description">{data.description}</div>
       <div className="project-card-tags">
-        <span className="project-card-language">
-          <span
-            className="project-card-language-badge"
-            style={{
-              backgroundColor: LanguageColor[data.tags[0]],
-            }}
-          />
-          {data.tags[0]}
-        </span>
+        <LanguageBadge tag={data.tags[0]} />
       </div>
     </div>
   );
