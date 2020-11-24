8d88d3a948b36bf6eff5c5a577a1e8c3dfd2c436
startergate <startergate36@gmail.com>
2020-11-24 18:26:51 +0900

    Updates

--- src/enums/langs.ts
+++
@@ -1,7 +0,0 @@
-enum LanguageColor {
-  Python = '#3572A5',
-  TypeScript = '#2B7489',
-  JavaScript = '#F1E05A',
-}
-
-export default LanguageColor;
--- src/util/classToggler.ts
+++
@@ -1,7 +0,0 @@
-const toggle = (target, className) => {
-  target.classList.contains(className)
-    ? target.classList.remove(className)
-    : target.classList.add(className);
-};
-
-export default toggle;
