62111667ff94cf461495d0fd7a78d5cc2d388f08
startergate <startergate36@gmail.com>
2020-11-25 05:27:39 +0900

    add: basic project detail

--- src/components/projects/detail.tsx
+++ src/components/projects/detail.tsx
@@ -1,28 +1,33 @@
 import * as React from 'react';
 
-import LanguageColor from '../../enums/langs';
 import Image from '../images/image';
+import LanguageBadge from './projectLanguage';
 
 import './overlay.css';
 import './detail.css';
-import { graphql, useStaticQuery } from 'gatsby';
 
 const ProjectDetail = ({ data }) => (
   <div className="overlay-item project-detail" id={'overlay-' + data.id}>
-    <Image className="project-detail-icon" src={data.imgSrc} />
-    <h3 className="project-detail-name">{data.name}</h3>
-    <div className="project-card-description">{data.description}</div>
-    <div className="project-card-tags">
-      <span className="project-card-language">
-        <span
-          className="project-card-language-badge"
-          style={{
-            backgroundColor: LanguageColor[data.tags[0]],
-          }}
-        />
-        {data.tags[0]}
-      </span>
+    <div className="project-detail-title">
+      <Image className="project-detail-icon" src={data.imgSrc} />
+      <h2 className="project-detail-name">{data.name}</h2>
+      <div className="project-card-description">{data.description}</div>
+      <div className="project-card-tags">
+        {data.tags.map((value) => (
+          <LanguageBadge tag={value} />
+        ))}
+      </div>
     </div>
+    {data.contents ? (
+      <div className="project-detail-content">
+        {data.contents?.map(({ title, text }) => (
+          <div className="project-detail-content-item">
+            <h5>{title}</h5>
+            {text}
+          </div>
+        ))}
+      </div>
+    ) : null}
   </div>
 );
 
