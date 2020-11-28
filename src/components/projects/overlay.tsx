1765b02817ce102c74b8f7e7105441df1f0feca0
startergate <startergate36@gmail.com>
2020-11-29 07:27:24 +0900

    add: press esc to close overlay

--- src/components/projects/overlay.tsx
+++ src/components/projects/overlay.tsx
@@ -3,21 +3,37 @@ import ProjectDetail from './detail';
 
 import './overlay.css';
 
-const Overlay = ({ data }) => (
-  <div
-    className="project-overlay"
-    onClick={(event) => {
-      event.currentTarget.classList.remove('active');
-      document.querySelector('body').classList.remove('disabled');
-      document.querySelectorAll('.overlay-item').forEach((element) => {
-        element.classList.remove('active');
-      });
-    }}
-  >
-    {data.map((project) => (
-      <ProjectDetail data={project} />
-    ))}
-  </div>
-);
+const Overlay = ({ data }) => {
+  document.addEventListener('keydown', (event) => {
+    console.log(event)
+    if (event.keyCode == 27 || event.which == 27) {
+      const overlay = document.querySelector('.project-overlay');
+      overlay.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+      }))
+    }
+  })
+
+  return (
+    <div
+      className="project-overlay"
+      onClick={(event) => {
+        console.log(event)
+        if (event.target !== event.currentTarget) return;
+        event.currentTarget.classList.remove('active');
+        document.querySelector('body').classList.remove('disabled');
+        document.querySelectorAll('.overlay-item').forEach((element) => {
+          element.classList.remove('active');
+        });
+      }}
+    >
+      {data.map((project) => (
+        <ProjectDetail data={project}/>
+      ))}
+    </div>
+  )
+};
 
 export default Overlay;
