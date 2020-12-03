524eacc9016f2fedbef1857e8d2005503c7a9e71
startergate <startergate36@gmail.com>
2020-12-04 02:02:20 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-84264cb3cba6efa872b1.js"
+    "url": "webpack-runtime-25351e6ed4ef3114ae65.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "684770aff66ff1770894257f99eea83c"
+    "revision": "523e58d6ecd1f4ca3604ad8717b6eaa6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
