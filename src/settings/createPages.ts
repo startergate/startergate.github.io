cfd70cff7522b92bd887e43af2ec8da0f42f0a37
Hoseung Choi <me@startergate.dev>
2026-01-24 19:26:15 +0900

    fix: build

--- src/settings/createPages.ts
+++ src/settings/createPages.ts
@@ -1,13 +1,12 @@
 import { resolve } from 'path';
+import { GatsbyNode } from 'gatsby';
 
-export const createPages = async ({ graphql, actions }) => {
+export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
-  const result = await graphql(`
-    query {
-      allProjectsJson(
-        sort: { fields: [isHighlighted, orderLevel, name], order: [DESC, ASC] }
-      ) {
+  const result = await graphql<{allProjectsJson: {nodes: any[]}}>(`
+    {
+      allProjectsJson(sort: [{isHighlighted: DESC}, {orderLevel: ASC}, {name: ASC}]) {
         nodes {
           id
           name
@@ -19,7 +18,7 @@ export const createPages = async ({ graphql, actions }) => {
     throw result.errors;
   }
 
-  result.data.allProjectsJson.nodes.map((data) => {
+  result.data?.allProjectsJson?.nodes?.map((data) => {
     createPage({
       path: `project/${data.name}/`,
       component: resolve(__dirname, '../pages/projects.tsx'),
